9c5273056e9c84cc83c29115c1801484b4c9fd16
Abhishek Pundir <ask.pundir0224@gmail.com>
2025-10-24 18:51:43 +0530

    almost done with the tabs in the RightWidget1 component

--- src/RightWidget1.jsx
+++ src/RightWidget1.jsx
@@ -1,7 +1,31 @@
+import { useState } from "react";
+import QuestionMarkIcon from "./assets/questionmark.svg?react";
+import "./RightWidget1.css";
 function RightWidget1() {
+  const [activeTab, setActiveTab] = useState("about-me");
   return (
-    <div className="widget">
-     
+    <div className="widget p-2 lg:p-3">
+      <QuestionMarkIcon className="absolute top-3 left-2 lg:top-4 lg:left-3 w-4 h-auto lg:w-[22px] lg:h-auto" />
+      <div className="tabs-container  font-['Poppins', sans-serif]">
+        <button
+          className={`tab ${activeTab === "about-me" ? "active-tab" : ""}`}
+          onClick={() => setActiveTab("about-me")}
+        >
+          <span>About Me</span>
+        </button>
+        <button
+          className={`tab ${activeTab === "experiences" ? "active-tab" : ""}`}
+          onClick={() => setActiveTab("experiences")}
+        >
+          <span>Experiences</span>
+        </button>
+        <button
+          className={`tab ${activeTab === "recommended" ? "active-tab" : ""}`}
+          onClick={() => setActiveTab("recommended")}
+        >
+          <span>Recommended</span>
+        </button>
+      </div>
     </div>
   );
 }
