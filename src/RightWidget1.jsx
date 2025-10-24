19f955f951b6c4248c62ec54e9693f3e9a7abf51
Abhishek Pundir <ask.pundir0224@gmail.com>
2025-10-24 20:34:59 +0530

    completed the tabs

--- src/RightWidget1.jsx
+++ src/RightWidget1.jsx
@@ -1,24 +1,43 @@
 import { useState } from "react";
 import QuestionMarkIcon from "./assets/questionmark.svg?react";
 import "./RightWidget1.css";
+
 function RightWidget1() {
   const [activeTab, setActiveTab] = useState("about-me");
+
   return (
-    <div className="widget p-2 lg:p-3">
+    <div className="widget p-2 lg:p-3 relative">
       <QuestionMarkIcon className="absolute top-3 left-2 lg:top-4 lg:left-3 w-4 h-auto lg:w-[22px] lg:h-auto" />
-      <div className="tabs-container  font-['Poppins', sans-serif]">
+
+      <div className="tabs-container font-['Poppins', sans-serif] relative">
+        {/*Moving Active Background Indicator */}
+        <div
+          className="active-indicator"
+          style={{
+            transform:
+              activeTab === "about-me"
+                ? "translateX(0%)"
+                : activeTab === "experiences"
+                ? "translateX(100%)"
+                : "translateX(200%)",
+          }}
+        ></div>
+
+        {/* Tabs */}
         <button
           className={`tab ${activeTab === "about-me" ? "active-tab" : ""}`}
           onClick={() => setActiveTab("about-me")}
         >
           <span>About Me</span>
         </button>
+
         <button
           className={`tab ${activeTab === "experiences" ? "active-tab" : ""}`}
           onClick={() => setActiveTab("experiences")}
         >
           <span>Experiences</span>
         </button>
+
         <button
           className={`tab ${activeTab === "recommended" ? "active-tab" : ""}`}
           onClick={() => setActiveTab("recommended")}
