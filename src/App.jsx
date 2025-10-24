fdb2fe20717cabcef20ff6bd93da30db61c165a2
Abhishek Pundir <ask.pundir0224@gmail.com>
2025-10-24 13:33:42 +0530

    made layouts for widgets

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,27 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+// import { useState } from "react";
+// import reactLogo from "./assets/react.svg";
+// import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
   return (
     <>
-      <div className="border-[#96BEE7] border w-[50%] h-[80%] rounded-[27px] bg-[#616161]"></div>
-      <div className="w-[43%] h-[81.8%] rounded-2xl flex flex-col items-center gap-3.5 relative top-[9px]">
-        <div className="rounded-[14px] bg-linear-to-r from-[#4A4E54] to-[#363C43] w-full  h-[50%]"></div>
-        <div className="h-[3px] bg-linear-to-r from-[#4A4E54] to-[#363C43] w-[85%] rounded-xs"></div>
-        <div className="rounded-[14px] bg-linear-to-r from-[#4A4E54] to-[#363C43] w-full  h-[50%]"></div>
-        <div className="h-[3px] bg-linear-to-r from-[#4A4E54] to-[#363C43] w-[85%] rounded-xs"></div>
+      {/* Left widget */}
+      <div className="border-[#96bee7] border w-[50%] h-[80%] rounded-[27px] bg-[#616161d1]"></div>
+
+      {/* Right widgets container */}
+      <div className="w-[43%] h-[82.3%] rounded-2xl flex flex-col items-center gap-[17px] relative top-2.5">
+        {/* Right widget-1*/}
+        <div className="widget"></div>
+
+        {/* Widget separator-1 */}
+        <div className="widget-separator"></div>
+
+        {/* Right widget-2*/}
+        <div className="widget"></div>
+
+        {/* Widget separator-2 */}
+        <div className="widget-separator"></div>
       </div>
     </>
   );
