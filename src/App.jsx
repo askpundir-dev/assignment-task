c1278d080f304e73af20b827a6919f9e715f38bb
Abhishek Pundir <ask.pundir0224@gmail.com>
2025-10-25 14:49:20 +0530

    removed unnecessary lines

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,3 @@
-// import { useState } from "react";
-// import reactLogo from "./assets/react.svg";
-// import viteLogo from "/vite.svg";
 import RightWidget1 from "./RightWidget1";
 import RightWidget2 from "./RightWidget2";
 import "./App.css";
