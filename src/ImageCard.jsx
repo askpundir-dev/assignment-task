bffc27da8b377d43a50ab541003ba864d9a6c224
Abhishek Pundir <ask.pundir0224@gmail.com>
2025-10-26 16:30:40 +0530

    made image card component

--- src/ImageCard.jsx
+++ src/ImageCard.jsx
@@ -1,6 +1,7 @@
 const ImageCard = ({ src, alt }) => {
   return (
-    <div className="image-container h-[90%] w-1/3 rounded-3xl grayscale-100 hover:grayscale-0 transition-all duration-300 ease-in-out hover:scale-[1.1] hover:-rotate-1 hover:-translate-1 hover:shadow-[-5px_-3px_30px_-10px_#96bee7,4px_5px_30px_5px_#000000]">
+    <div className="card grayscale-100 hover:grayscale-0 transition-all duration-300 ease-in-out hover:scale-[1.1] hover:-rotate-1 hover:-translate-1 hover:shadow-[0px_0px_10px_-3px_#96bee7,1px_2px_20px_2px_#000000]
+">
       <img
         className="w-full h-full rounded-3xl object-cover object-center"
         src={src}
