3ff9a91c6b7ed541f79d702a6f28513e82fe22a0
Abhishek Pundir <ask.pundir0224@gmail.com>
2025-10-26 17:11:06 +0530

    assignment completed

--- src/ImageCard.jsx
+++ src/ImageCard.jsx
@@ -1,7 +1,9 @@
 const ImageCard = ({ src, alt }) => {
   return (
-    <div className="card grayscale-100 hover:grayscale-0 transition-all duration-300 ease-in-out hover:scale-[1.1] hover:-rotate-1 hover:-translate-1 hover:shadow-[0px_0px_10px_-3px_#96bee7,1px_2px_20px_2px_#000000]
-">
+    <div
+      className="card grayscale-100 hover:grayscale-0 transition-all duration-300 ease-in-out hover:scale-[1.1] hover:-rotate-1 hover:-translate-1 hover:shadow-[0px_0px_12px_-3px_#96bee7,1px_2px_5px_2px_#000000]
+"
+    >
       <img
         className="w-full h-full rounded-3xl object-cover object-center"
         src={src}
